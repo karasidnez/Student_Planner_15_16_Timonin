77aa92186e4187926800444c48dd9f9daebd7b62
karasidnez <karasdnez@gmail.com>
2026-03-24 10:21:47 +0300

    Step 3: Create Subject data model and sample data

---
+++ app/src/main/java/com/timonin/student_planner/data/Subject.kt
@@ -0,0 +1,60 @@
+package com.timonin.student_planner.data
+
+data class Subject(
+    val id: String,
+    val name: String,
+    val professor: String,
+    val credits: Int,
+    val currentGrade: String,
+    val description: String
+)
+val sampleSubject = listOf(
+    Subject(
+        id = "1",
+        name = "Разработка мобильных приложений",
+        professor = "Леонтьев Д.А.",
+        credits = 91,
+        currentGrade = "Отлично",
+        description = "Разработка нативных Android-приложений с использованием Kotlin и современных компонентов Jetpack Compose. Изучение архитектурных паттернов MVVM, работы с локальными базами данных и сетевыми API."
+    ),
+    Subject(
+        id = "2",
+        name = "Разработка мобильных приложений",
+        professor = "Леонтьев Д.А.",
+        credits = 91,
+        currentGrade = "Отлично",
+        description = "Разработка нативных Android-приложений с использованием Kotlin и современных компонентов Jetpack Compose. Изучение архитектурных паттернов MVVM, работы с локальными базами данных и сетевыми API."
+    ),
+    Subject(
+        id = "3",
+        name = "Системное программирование",
+        professor = "Токаев Т.И.",
+        credits = 75,
+        currentGrade = "Хорошо",
+        description = "Низкоуровневая разработка на языке Rust: управление памятью без сборщика мусора, многопоточность, работа с операционной системой и создание высокопроизводительных системных компонентов."
+    ),
+    Subject(
+        id = "4",
+        name = "Разработка мобильных приложений",
+        professor = "Леонтьев Д.А.",
+        credits = 91,
+        currentGrade = "Отлично",
+        description = "Разработка нативных Android-приложений с использованием Kotlin и современных компонентов Jetpack Compose. Изучение архитектурных паттернов MVVM, работы с локальными базами данных и сетевыми API."
+    ),
+    Subject(
+        id = "5",
+        name = "Разработка мобильных приложений",
+        professor = "Леонтьев Д.А.",
+        credits = 91,
+        currentGrade = "Отлично",
+        description = "Разработка нативных Android-приложений с использованием Kotlin и современных компонентов Jetpack Compose. Изучение архитектурных паттернов MVVM, работы с локальными базами данных и сетевыми API."
+    ),
+    Subject(
+        id = "6",
+        name = "Разработка мобильных приложений",
+        professor = "Леонтьев Д.А.",
+        credits = 91,
+        currentGrade = "Отлично",
+        description = "Разработка нативных Android-приложений с использованием Kotlin и современных компонентов Jetpack Compose. Изучение архитектурных паттернов MVVM, работы с локальными базами данных и сетевыми API."
+    ),
+)
